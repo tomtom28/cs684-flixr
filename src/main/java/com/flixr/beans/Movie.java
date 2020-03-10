fc95b61ce4e2479144f3aeceeddd28abef70a8ec
vdes9496 <vrajrakesh@gmail.com>
2020-03-09 22:00:16 -0400

    Add files via upload

--- src/main/java/com/flixr/beans/Movie.java
+++ src/main/java/com/flixr/beans/Movie.java
@@ -1,4 +1,61 @@
 package com.flixr.beans;
 
 public class Movie {
+    private int movieID;
+    private String moviename;
+    private String releasedate;
+    private String agerating;
+    private String actors;
+    private int runtime;
+    private String director;
+    private String writer;
+
+    public int getMovieID(){
+        return this.movieID;
+    }
+    public void setMovieID(int movieID){
+        this.movieID = movieID;
+    }
+    public String getMoviename(){
+        return this.moviename;
+    }
+    public void setMoviename(String moviename) {
+        this.moviename = moviename;
+    }
+    public String getReleasedate() {
+        return releasedate;
+    }
+    public void setReleasedate(String releasedate){
+        this.releasedate = releasedate;
+    }
+    public String getAgerating(){
+        return this.agerating;
+    }
+    public void setAgerating(String agerating){
+        this.agerating = agerating;
+    }
+    public String getActors(){
+        return this.actors;
+    }
+    public void setActors(String actors){
+        this.actors = actors;
+    }
+    public int getRuntime(){
+        return this.runtime;
+    }
+    public void setRuntime(int runtime){
+        this.runtime = runtime;
+    }
+    public String getDirector(){
+        return this.director = director;
+    }
+    public void setDirector(){
+        this.director = director;
+    }
+    public String getWriter(){
+        return this.writer;
+    }
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
 }
--- src/main/java/com/flixr/beans/User.java
+++ src/main/java/com/flixr/beans/User.java
@@ -1,4 +1,47 @@
 package com.flixr.beans;
 
 public class User {
+    private int userID;
+    private String fullname;
+    private String password;
+    private String email;
+    private String date;
+    private String country;
+
+    public int getUserID(){
+        return this.userID;
+    }
+    public void setUserID(int userID){
+        this.userID = userID;
+    }
+    public String getFullname(){
+        return this.fullname;
+    }
+    public void setFullname(String fullname){
+        this.fullname = fullname;
+    }
+    public String getPassword(){
+        return this.password;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getDate(){
+        return this.date;
+    }
+    public void setDate(String date){
+        this.date = date;
+    }
+    public String getCountry(){
+        return this.country;
+    }
+    public void SetCountry(String country){
+        this.country = country;
+    }
 }
