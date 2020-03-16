26e49fa03926d5761f8dd4781d16c9c0df413516
albert chingis <albertchingis@alberts-MBP.fios-router.home>
2020-03-16 10:38:09 -0400

    fix star position

--- frontend/app.js
+++ frontend/app.js
@@ -1,13 +1,15 @@
-//jshint esversion:6
+     //jshint esversion:6
 var express=require("express");
 var bodyParser=require("body-parser");
+var https=require("https");
+var http=require("http");
 
 var app=express();
 app.set("view engine","ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
-var movie_list=[{title:"dog1",image:"http://all4desktop.com/data_images/original/4243865-dog.jpg",description:"gold hair"},{title:"dog2",image:"http://all4desktop.com/data_images/original/4243865-dog.jpg",description:"gold hair"},{title:"dog3",image:"http://all4desktop.com/data_images/original/4243865-dog.jpg",description:"gold hair"}];
+var userid;
 
 app.get("/",function(req,res){
   res.render("home");
@@ -20,9 +22,18 @@ app.get("/signin",function(req,res){
 app.get("/signup",function(req,res){
   res.render("signup");
 });
-    
+
+
 app.get("/rating",function(req,res){
-  res.render("rating",{list:movie_list});
+  http.get("http://localhost:3001/api",function(getback){
+
+    getback.on("data",function(data){
+      var i=JSON.parse(data);
+      console.log(i);
+    });
+
+  });
+  res.render("rating");
 });
 
 app.get("/recommend",function(req,res){
@@ -42,5 +53,5 @@ app.get("/contact",function(req,res){
 });
 
 app.listen(process.env.PORT||3000, function() {
-  console.log("Server started on port 3003~");
+  console.log("Server started on port 3000~");
 });
