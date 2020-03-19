a752b6062b0ecc2d2606f88e1aae4a6f120fcdac
vdes9496 <vrajrakesh@gmail.com>
2020-03-18 22:30:49 -0400

    Add files via upload

--- src/main/java/com/flixr/dao/UserDAO.java
+++ src/main/java/com/flixr/dao/UserDAO.java
@@ -1,5 +1,31 @@
+//Vraj Desai
 package com.flixr.dao;
+import com.flixr.exceptions.DAOException;
+import java.sql.*;
+import java.util.*;
 
 public class UserDAO {
-    // todo
+    // Login
+    public void loginUser(String email, String password){
+        try{
+            Connection conn = DriverManager.getConnection(DB_CONNECTION_URL, DB_USERNAME, DB_PASSWORD);
+            PreparedStatement stmt = conn.prepareStatement("SELECT EmailAddress, UserPassword FROM Users");
+            stmt.setString(1,email);
+            stmt.setString(2,password);
+            ResultSet resultSet = stmt.executeQuery();
+            if(resultSet.next()){
+                HttpSession session = request.getSession();
+                session.setAttribute("EmailAddress", email);
+                //response.sendRedirect("Home.jsp");
+            }else{
+                JOptionPane.showMessageDialog(null, "Incorrect Email or Password");
+            }
+            resultSet.close();
+            conn.close();
+        }
+    }
+    //Sign up
+    public void signupUser(String name, String email, String password, int age, String country){
+
+    }
 }
