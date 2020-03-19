2634f1a3415838ec83975361637f3db312ea3acc
vdes9496 <vrajrakesh@gmail.com>
2020-03-18 22:27:39 -0400

    Add files via upload

--- src/main/java/com/flixr/application/ApplicationController.java
+++ src/main/java/com/flixr/application/ApplicationController.java
@@ -1,4 +1,41 @@
+/* Vraj Desai */
+/*
 package com.flixr.application;
 
+import com.flixr.beans.MovieWithPrediction;
+import com.flixr.beans.UserSubmission;
+import com.flixr.dao.EngineDAO;
+import com.flixr.dao.UserDAO;
+import com.flixr.dao.MovieDAO;
+import com.flixr.dao.RatingDAO;
+import com.flixr.dao.PredictionDAO;
+import com.flixr.engine.RecommendationEngine;
+import com.flixr.exceptions.DAOException;
+import com.flixr.exceptions.EngineException;
+
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 public class ApplicationController {
+
+    public ApplicationController(){
+
+    }
+    UserDAO userDAO = new UserDAO();
+    UserSubmission userSubmission = engineDAO.getUserSubmission(userId);
+    List<Users> = new userDAO.loginUser();
+    List<Registered> new userDAO.signupUser();
+
+    MovieDAO movieDAO = new MovieDAO();
+    List<Movies> movies = Movie.getNextMovie(int userId);
+
+    RatingDAO ratingDAO = new RatingDAO();
+    List<StatsRating> = new ratingDAO.getMovieStatsByAverageRating();
+    List<Title> = new ratingDAO.getMovieStatsByTitle();
+    List<RatingCount> = new ratingDAO.getMovieStatsByRatingCount();
+
 }
+
