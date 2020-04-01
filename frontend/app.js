a435bd5bd7551550c18f08de08c107c2b9b7fad7
albert chingis <albertchingis@alberts-MBP.fios-router.home>
2020-04-01 19:51:34 -0400

    user session done. search bar api need to do

--- frontend/app.js
+++ frontend/app.js
@@ -5,21 +5,86 @@ var bodyParser=require("body-parser");
 var https=require("https");
 var http=require("http");
 var request = require('request');
+var session = require('express-session');
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
 
 var app=express();
 app.set("view engine","ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
-var user_email="g@m.com";
+
+app.use(session({
+  secret: "hello.",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+  // User.findById(id, function(err, user) {
+  //   done(err, user);
+  // });
+});
+
+app.use(function(req,res,next){
+  //res.locals is the objects to pass in, req.user return current user.
+  res.locals.currentUser=req.user;
+  next();
+});
+
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+  },
+  function(username, password, done) {
+    //post signin info to backend
+    //user_email=username;
+
+    request.post(
+      url+"/signin",
+      {form:{email:username,password:password}},
+      function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+              console.log(body);
+          }
+      }
+    );
+    var user
+    ={username:username,password:password,movie_rate_count:0};
+    http.get(url+"/checkstatus/"+username,function(getback){
+      getback.on("data",function(data){
+        var i=JSON.parse(data);
+        console.log(i);
+        user_id=i.user_id;
+        console.log(user_id);
+
+        if(i.status=="off"){
+          return done(null,false);
+        }
+        else if(i.status=="on"){
+          return done(null,user);
+        }
+      });
+    });
+
+  }
+));
+
 var url="http://localhost:3001";
-var status;
-var movie_rate_count=0;
-var user_id="0";
 
 //home page
 app.get("/",function(req,res){
-  res.render("home");
+  if (req.isAuthenticated()){
+    res.render("home",{message:"on"});
+  } else {
+    res.render("home",{message:"off"});
+  }
 });
 
 //get the signin page
@@ -27,22 +92,11 @@ app.get("/signin",function(req,res){
   res.render("signin");
 });
 
-//post signin info to sever
-app.post("/signin",function(req,res){
-  //console.log(req.body);
-  user_email=req.body.email;
-  request.post(
-    url+"/signin",
-    {form:req.body},
-    function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body);
-
-        }
-    }
-  );
-  res.redirect("/checkstatus");
-});
+app.post('/signin',
+  passport.authenticate('local',
+  {    successRedirect: '/',
+  failureRedirect: '/signin' })
+);
 
 //get signup page
 app.get("/signup",function(req,res){
@@ -51,7 +105,7 @@ app.get("/signup",function(req,res){
 
 //post signup info to server
 app.post("/signup",function(req,res){
-  user_email=req.body.email;
+
   console.log(req.body);
   request.post(
     url+"/signup",
@@ -59,27 +113,46 @@ app.post("/signup",function(req,res){
     function (error, response, body) {
         if (!error && response.statusCode == 200) {
             console.log(body);
-            res.redirect("/checkstatus");
+
         }
     }
   );
-  res.redirect("/checkstatus");
+  //res.redirect("/");
+  var user
+  ={username:req.body.email,password:req.body.password,movie_rate_count:0};
+  req.login(user, function(err) {
+  if (err) { return next(err); }
+  return res.redirect('/' );
+  });
 });
 
 //logout
 app.get("/logout",function(req,res){
-  http.get(url+"/logout/"+user_id,function(getback){
-    getback.on("data",function(data){
-      var i=JSON.parse(data);
-      console.log(i);
+  if (req.isAuthenticated()){
+    http.get(url+"/checkstatus/"+req.user.username,function(getback){
+      getback.on("data",function(data){
+        var i=JSON.parse(data);
+
+        http.get(url+"/logout/"+i.user_id,function(getback){
+          getback.on("data",function(data){
+            var i2=JSON.parse(data);
+            console.log(i2);
+          });
+        });
+
+      });
     });
-  });
-  res.redirect("/");
+    req.logout();
+    res.redirect("/");
+  } else {
+    res.render("home",{message:"off"});
+  }
+
 });
 
 //check statuse
 app.get("/checkstatus",function(req,res){
-  http.get(url+"/checkstatus/"+user_email,function(getback){
+  http.get(url+"/checkstatus/"+req.user.username,function(getback){
     getback.on("data",function(data){
       var i=JSON.parse(data);
       console.log(i);
@@ -92,23 +165,35 @@ app.get("/checkstatus",function(req,res){
 
 //get the rating data
 app.get("/rating",function(req,res){
+  if (req.isAuthenticated()){
+    http.get(url+"/checkstatus/"+req.user.username,function(getback){
+      getback.on("data",function(data){
+        var i=JSON.parse(data);
+        console.log(req.user.movie_rate_count);
 
-  http.get(url+"/rating/"+user_id+"/"+movie_rate_count,function(getback){
-    getback.on("data",function(data){
-      var i=JSON.parse(data);
-    //  console.log(i);
-      res.render("rating",{movie:i});
+        http.get(url+"/rating/"+i.user_id+"/"+req.user.movie_rate_count,function(getback){
+          getback.on("data",function(data){
+            var i1=JSON.parse(data);
+          //  console.log(i);
+            res.render("rating",{movie:i1});
+          });
+
+        });
+      });
     });
-  });
+  } else {
+    res.render("signin");
+  }
+
 });
 
 //rating data go previous
 app.get("/rating/prev",function(req,res){
-    if(movie_rate_count-1<0){
-      movie_rate_count=0;
+    if(req.user.movie_rate_count-1<0){
+      req.user.movie_rate_count=0;
       res.redirect("/rating");
     }else{
-      movie_rate_count=movie_rate_count-1;
+      req.user.movie_rate_count=req.user.movie_rate_count-1;
       res.redirect("/rating");
     }
 
@@ -116,7 +201,7 @@ app.get("/rating/prev",function(req,res){
 
 //rating data go next
 app.get("/rating/next",function(req,res){
-    movie_rate_count=movie_rate_count+1;
+    req.user.movie_rate_count=req.user.movie_rate_count+1;
     res.redirect("/rating");
 });
 
@@ -137,21 +222,30 @@ app.post("/rating/:id",function(req,res){
     grade=5;
   }
 
-  var object={
-    user_id:user_id,
-    movie_id:req.params.id,
-    grade:grade
-  };
+  http.get(url+"/checkstatus/"+req.user.username,function(getback){
+    getback.on("data",function(data){
+      var i=JSON.parse(data);
 
-  request.post(
-    url+"/rating",
-    {form:object},
-    function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body);
+      var object={
+        user_id:i.user_id,
+        movie_id:req.params.id,
+        grade:grade
+      };
+
+      request.post(
+        url+"/rating",
+        {form:object},
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body);
+            }
         }
-    }
-  );
+      );
+
+    });
+  });
+
+
 
   res.redirect("/rating/next");
 });
@@ -159,34 +253,42 @@ app.post("/rating/:id",function(req,res){
 //get the recommended movie from server
 app.get("/recommend/:sort_type",function(req,res){
 
-  http.get(url+"/recommend/"+user_id+"/"+req.params.sort_type,function(getback){
+  if (req.isAuthenticated()){
+    http.get(url+"/checkstatus/"+req.user.username,function(getback){
+      getback.on("data",function(data){
+        var i1=JSON.parse(data);
 
-    getback.on("data",function(data){
-      var i=JSON.parse(data);
-      //console.log(i);
-      console.log(i.length);
+        http.get(url+"/recommend/"+i1.user_id+"/"+req.params.sort_type,function(getback){
 
-      var perPage=12;
-      var pageQuery=parseInt(req.query.page);
-      var pageNumber=pageQuery?pageQuery:1;
+          getback.on("data",function(data){
+            var i=JSON.parse(data);
+            //console.log(i);
+            console.log(i.length);
 
-      pageQuery--;
+            var perPage=12;
+            var pageQuery=parseInt(req.query.page);
+            var pageNumber=pageQuery?pageQuery:1;
 
-      var items=
-      i.slice(perPage*pageQuery,perPage*pageQuery+perPage);
+            pageQuery--;
 
-      res.render("recommend",{
-        items:items,
-        current:pageNumber,
-        pages:Math.ceil(i.length/perPage),
-        type:req.params.sort_type
+            var items=
+            i.slice(perPage*pageQuery,perPage*pageQuery+perPage);
+
+            res.render("recommend",{
+              items:items,
+              current:pageNumber,
+              pages:Math.ceil(i.length/perPage),
+              type:req.params.sort_type
+            });
+
+          });
+        });
+
       });
-
     });
-  });
-
-
-
+  } else {
+    res.render("signin");
+  }
 });
 
 //get the admin page
@@ -252,6 +354,12 @@ app.get("/about",function(req,res){
 //contact page
 app.get("/contact",function(req,res){
   res.render("contact");
+});
+
+app.post("/rating/search",function(req,res){
+  console.log(req.body.search);
+
+  res.redirect("/rating");
 });
 
 app.listen(process.env.PORT||3000, function() {
