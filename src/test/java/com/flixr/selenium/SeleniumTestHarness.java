3ce9e67a75bfc26e05cb6de696979ed692cc831e
tomtom28 <tomthompson828@gmail.com>
2020-04-23 00:24:39 -0400

    added test driver and test cases for full systems testing

--- src/test/java/com/flixr/selenium/SeleniumTestHarness.java
+++ src/test/java/com/flixr/selenium/SeleniumTestHarness.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,7 +14,8 @@ import org.openqa.selenium.remote.RemoteWebElement;
 
 import static com.flixr.selenium.helpers.SeleniumConstants.APP_URL;
 import static com.flixr.selenium.helpers.SeleniumConstants.WEB_DRIVER_LOCATION;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Selenium Test Harness for Full System Testing
@@ -45,6 +47,7 @@ public class SeleniumTestHarness {
         seleniumTestDriver = new SeleniumTestDriver();
     }
 
+
     /**
      * @author Thomas Thompson
      *
@@ -72,16 +75,17 @@ public class SeleniumTestHarness {
         }
     }
 
+
     /**
      * @author Thomas Thompson
-     * Verify that Admin User is able to see Admin Page
+     * Verify that the Admin User is able to see link to Admin Page
      */
     @Test
-    void testAdminAccess() {
+    void testAdminCanAccessAdminPage() {
 
         // Admin Credentials
-        String adminEmail = "admin@flixr.com";
-        String adminPassword = "admin";
+        String adminEmail = seleniumTestDriver.getAdminEmail();
+        String adminPassword = seleniumTestDriver.getAdminPassword();
 
         try {
             // Open chrome browser
@@ -90,20 +94,161 @@ public class SeleniumTestHarness {
             // Login User
             seleniumTestDriver.loginUser(webDriver, adminEmail, adminPassword);
 
-            // Check that "Admin" exists in the sidebar
-            WebElement sideBar = webDriver.findElement(By.id("sidebar"));
-            WebElement adminLink = ((RemoteWebElement) sideBar).findElementByLinkText("Admin");
-
-            // Navigate to "Admin" page
-            adminLink.click();
+            // Click to "Admin" page
+            seleniumTestDriver.clickSideBarLinkByName(webDriver, "Admin");
 
             // Close chrome browser
             webDriver.quit();
 
         } catch(Exception e) {
             e.printStackTrace();
-            fail("Unable to Load HomePage: " + e.getMessage());
+            fail("Error: " + e.getMessage());
         }
     }
+
+    /**
+     * @author Thomas Thompson
+     * Verify that non-Admin User is NOT able to see link to Admin Page
+     */
+    @Test
+    void testNonAdminUserCannotAccessAdminPage() {
+
+        // User Credentials
+        String userEmail = seleniumTestDriver.getDefaultUserEmail();
+        String userPassword = seleniumTestDriver.getDefaultUserPassword();
+
+        try {
+            // Open chrome browser
+            WebDriver webDriver = new ChromeDriver();
+
+            // Login User
+            seleniumTestDriver.loginUser(webDriver, userEmail, userPassword);
+
+            // Check that "Admin" does NOT exist in the sidebar
+            assertThrows(NoSuchElementException.class, () ->  seleniumTestDriver.clickSideBarLinkByName(webDriver, "Admin"));
+
+            // Close chrome browser
+            webDriver.quit();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+            fail("Error: " + e.getMessage());
+        }
+    }
+
+
+    /**
+     * @author Thomas Thompson
+     * Verify that a user is able to search for a given movie title
+     */
+    @Test
+    void testMovieRatingSearchBox() {
+
+        // User Credentials
+        String userEmail = seleniumTestDriver.getDefaultUserEmail();
+        String userPassword = seleniumTestDriver.getDefaultUserPassword();
+
+        // Search Movie Title
+        String movieName = "The Magnificent Seven";
+
+        try {
+            // Open chrome browser
+            WebDriver webDriver = new ChromeDriver();
+
+            // Login User
+            seleniumTestDriver.loginUser(webDriver, userEmail, userPassword);
+
+            // Click on Ratings
+            seleniumTestDriver.waitXseconds(3); // wait 3 seconds
+            seleniumTestDriver.clickSideBarLinkByName(webDriver, "Rating");
+
+            // Search for "Magnificent Seven" Movie
+            seleniumTestDriver.waitXseconds(3); // wait 3 seconds
+            seleniumTestDriver.searchForMovieToRateByName(webDriver, movieName);
+
+            // Check if Movie Name Appeared
+            seleniumTestDriver.waitXseconds(3); // wait 3 seconds
+            String currentMovieTitleToRate = seleniumTestDriver.getCurrentMovieNameBeingRated(webDriver);
+            assertEquals(movieName, currentMovieTitleToRate);
+
+            // Close chrome browser
+            webDriver.quit();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+            fail("Error: " + e.getMessage());
+        }
+    }
+
+
+    /**
+     * @author Thomas Thompson
+     * Verify that an older user is able to search for a given R-rated movie title
+     * Verify that a younger user is NOT able to search for the same movie title (i.e. child blocks)
+     */
+    @Test
+    void testUnderAgedUserCannotAccessRestrictedMovieInSearchBox() {
+
+        // Adult User Credentials
+        String olderUserEmail = seleniumTestDriver.getDefaultUserEmail();
+        String olderUserPassword = seleniumTestDriver.getDefaultUserPassword();
+
+        // Young User Credentials
+        String youngUserEmail = seleniumTestDriver.getDefaultUnder18UserEmail();
+        String youngUserPassword = seleniumTestDriver.getDefaultUnder18UserPassword();
+
+        // Search Movie Title
+        String movieName = "Superbad"; // known R-rated movie in database
+
+        try {
+            // Open chrome browsers (1 per user)
+            WebDriver webDriverForOlderUser = new ChromeDriver();
+            WebDriver webDriverForYoungerUser = new ChromeDriver();
+
+            // Login both Users
+            seleniumTestDriver.loginUser(webDriverForYoungerUser, youngUserEmail, youngUserPassword);
+            seleniumTestDriver.loginUser(webDriverForOlderUser, olderUserEmail, olderUserPassword);
+
+            // Click on Ratings for both users
+            seleniumTestDriver.waitXseconds(3); // wait 3 seconds
+            seleniumTestDriver.clickSideBarLinkByName(webDriverForYoungerUser, "Rating");
+            seleniumTestDriver.clickSideBarLinkByName(webDriverForOlderUser, "Rating");
+
+            // Search for Movie for both users
+            seleniumTestDriver.waitXseconds(3); // wait 3 seconds
+            seleniumTestDriver.searchForMovieToRateByName(webDriverForYoungerUser, movieName);
+            seleniumTestDriver.searchForMovieToRateByName(webDriverForOlderUser, movieName);
+
+            // Movie name should NOT appear for Young User
+            seleniumTestDriver.waitXseconds(3); // wait 3 seconds
+            String currentMovieTitleOfYoungUser = seleniumTestDriver.getCurrentMovieNameBeingRated(webDriverForYoungerUser);
+            assertNotEquals(movieName, currentMovieTitleOfYoungUser); // NOT equals for younger user
+
+            // Movie name should appear for Older User
+            String currentMovieTitleOfOlderUser = seleniumTestDriver.getCurrentMovieNameBeingRated(webDriverForOlderUser);
+            assertEquals(movieName, currentMovieTitleOfOlderUser); // YES equals for older user
+
+            // Close BOTH chrome browsers
+            webDriverForYoungerUser.quit();
+            webDriverForOlderUser.quit();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+            fail("Error: " + e.getMessage());
+        }
+    }
+
+
+
+    // TODO ... KEEP ADDING TESTS...
+
+    // EXAMPLE: YOU CAN USE rateCurrentMovie() method from SeleniumTestDriver
+
+    // Example: You can test other pages like "About" or "Recommend" using:
+    // seleniumTestDriver.clickSideBarLinkByName(webDriver, "INSERT LINK NAME HERE");
+
+    // TIP:
+    // User  seleniumTestDriver.waitXseconds(3); to wait for 3 (or more) seconds for webpages to load
+
 
 }
--- src/test/java/com/flixr/selenium/helpers/SeleniumTestDriver.java
+++ src/test/java/com/flixr/selenium/helpers/SeleniumTestDriver.java
@@ -1,12 +1,58 @@
 package com.flixr.selenium.helpers;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import com.flixr.exceptions.TestException;
+import org.openqa.selenium.*;
+import org.openqa.selenium.remote.RemoteWebElement;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.flixr.selenium.helpers.SeleniumConstants.APP_URL;
 
 public class SeleniumTestDriver {
+
+    // Default Admin Credentials
+    private String adminEmail = "admin@flixr.com";
+    private String adminPassword = "admin";
+
+    public String getAdminEmail() {
+        return adminEmail;
+    }
+    public String getAdminPassword() {
+        return adminPassword;
+    }
+
+    // Default User Credentials (over 18)
+    private String defaultUserEmail = "jsmith@email.com";
+    private String defaultUserPassword = "password";
+
+    public String getDefaultUserEmail() {
+        return defaultUserEmail;
+    }
+    public String getDefaultUserPassword() {
+        return defaultUserPassword;
+    }
+
+    // Default Under-aged User Credentials (under 18)
+    private String defaultUnder18UserEmail = "kid@email.com";
+    private String defaultUnder18UserPassword = "password";
+
+    public String getDefaultUnder18UserEmail() {
+        return defaultUnder18UserEmail;
+    }
+    public String getDefaultUnder18UserPassword() {
+        return defaultUnder18UserPassword;
+    }
+
+    /**
+     * @author Thomas Thompson
+     * Force the Test Driver to wait X number of seconds
+     * @param seconds       Number of Seconds to Wait
+     */
+    public void waitXseconds(int seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {}
+    }
 
     /**
      * @author Thomas Thompson
@@ -18,6 +64,8 @@ public class SeleniumTestDriver {
 
         // Load Sign Up Page
         webDriver.get(APP_URL + "/signin");
+
+        int DEBUG0 = 0;
 
         // Add Email
         WebElement emailForm = webDriver.findElement(By.id("inputEmail"));
@@ -35,5 +83,80 @@ public class SeleniumTestDriver {
 
         int DEBUG2 = 0;
     }
+
+
+    /**
+     * @author Thomas Thompson
+     * Clicks the selected link (by name) in the navbar
+     * @param webDriver     Reference to Selenium Web Driver
+     * @param nameOfLink    Name Of Link in SideBar (ex. "Admin", or "Rating"
+     */
+    public void clickSideBarLinkByName(WebDriver webDriver, String nameOfLink) throws NoSuchElementException {
+        // Click selected link in sidebar
+        WebElement sideBar = webDriver.findElement(By.id("sidebar"));
+        WebElement linkElement = ((RemoteWebElement) sideBar).findElementByLinkText(nameOfLink);
+        linkElement.click();
+    }
+
+
+    /**
+     * @author Thomas Thompson
+     * Search a Movie on the Rating Page
+     * @param webDriver         Reference to Selenium Web Driver
+     * @param searchMovieName   Movie Name you want to search
+     */
+    public void searchForMovieToRateByName(WebDriver webDriver, String searchMovieName) {
+
+        // Write Movie to search box
+        WebElement searchBox = webDriver.findElement(By.name("search"));
+        searchBox.sendKeys(searchMovieName);
+
+        int DEBUG1 = 0;
+
+        // Click Movie search button
+        WebElement movieSearchBtn = webDriver.findElement(By.className("btn-light"));
+        movieSearchBtn.click();
+
+    }
+
+    /**
+     * @author Thomas Thompson
+     * @param webDriver     Reference to Selenium Web Driver
+     * @return  Returns the Name of the Movie currently shown in the Ratings Page
+     */
+    public String getCurrentMovieNameBeingRated(WebDriver webDriver) {
+        // Movie Name (header) of tile in Rating page
+        WebElement titleMovieName = webDriver.findElement(By.tagName("h3"));
+        return titleMovieName.getText();
+    }
+
+    /**
+     * @author Thomas Thompson
+     * @param webDriver     Reference to Selenium Web Driver
+     * @param numberOfStars Number of Stars to Rate Current Movie
+     */
+    public void rateCurrentMovie(WebDriver webDriver, int numberOfStars) throws TestException {
+
+        // Ensure # of Stars is valid
+        if (numberOfStars < 1 || numberOfStars > 5) {
+            throw new TestException(new Exception("Invalid Number of Stars!"));
+        }
+
+        // Transform rating to star name
+        int starNumber = 5 - numberOfStars + 1;
+        String starName = "star" + starNumber;
+
+        // Select the correct Star to give the right rating
+        WebElement starBtn = webDriver.findElement(By.id(starName));
+        ((JavascriptExecutor) webDriver).executeScript("arguments[0].checked = true;", starBtn);
+
+        // Submit the Rating
+        this.waitXseconds(2); // wait 2 seconds
+        WebElement submitBtn = webDriver.findElement(By.className("btn-primary"));
+        submitBtn.click();
+
+        int DEBUG = 0;
+    }
+
 
 }
